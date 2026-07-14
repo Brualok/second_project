efa82bda28379bf33b16942a1e15f3b097ae9cd2
Bryalok <vasya.zalupin.77@mail.ru>
2026-07-14 21:36:35 +0500

    add hex_to_byte

--- include/getopt.h
+++ include/getopt.h
@@ -7,4 +7,16 @@
 #ifndef HEXDUMP_H
 #define HEXDUMP_H
 
+#include <stdio.h>
+#include <stddef.h>
+#include <sys/types.h>
+
+int hexdump_file(const char* filename, off_t offset, size_t size,
+    int group_size, int count_per_line);
+
+
+int hexdump_directory(const char* dirname, off_t offset, size_t size,
+    int group_size, int count_per_line);
+
+
 #endif
--- src/getopt.c
+++ src/getopt.c
@@ -5,3 +5,19 @@
 МК-101
 */
 #include "hexdump.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <limits.h>
+#include <unistd.h>
+
+/* Преобразование одного байта в два шестнадцатеричных символа */
+static void byte_to_hex(unsigned char b, char out[2]) {
+    static const char hexchars[] = "0123456789ABCDEF";
+    out[0] = hexchars[b >> 4];
+    out[1] = hexchars[b & 0x0F];
+}
